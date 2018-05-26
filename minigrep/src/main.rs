2629b368392a147629f409046dff7176ee142c36
koi <koriatoren@gmail.com>
2018-05-26 13:52:27 +0300

    config constructor returns wrapped result

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,10 +8,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &Vec<String>) -> Config {
+    fn new(args: &Vec<String>) -> Result<Config, &'static str> {
+        if args.len() != 3 {
+            return Err("wrong number of arguments (2 needed)")
+        }
         let pattern = args[1].clone();
         let filename = args[2].clone();
-        Config { pattern, filename }
+        Ok(Config { pattern, filename })
     }
 }
 
@@ -20,12 +23,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("args: {:?}", args);
 
-    let config = Config::new(&args); // parse_cli
+    let config = Config::new(&args).unwrap(); // parse_cli
 
     println!("searching: {}", config.pattern);
     println!("in file: {}", config.filename);
 
-    let mut f = File::open(config.filename).expect("couldn't find file");
+    let mut f = File::open(&config.filename).expect(
+        &format!("couldn't find file: '{}'", &config.filename)
+    );
     let mut text = String::new();
     f.read_to_string(&mut text);
     println!("content:\n{}", text);
