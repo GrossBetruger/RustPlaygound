c9fad02d5356095bf126e3abbcd7fe2c2d7bc6bf
koi <koriatoren@gmail.com>
2018-05-26 22:33:30 +0300

    refactored most logic to lib, added tests

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,75 @@
+extern crate regex;
+
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::error::Error;
+use regex::Regex;
+
+pub struct Config {
+    pattern: String,
+    filename: String
+}
+
+impl Config {
+    pub fn new(args: &Vec<String>) -> Result<Config, &'static str> {
+        if args.len() != 3 {
+            return Err("wrong number of arguments (2 needed)")
+        }
+        let pattern = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { pattern, filename })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    println!("searching: {}", config.pattern);
+    println!("in file: {}", config.filename);
+
+    let mut f = File::open(&config.filename)?;
+    let mut text = String::new();
+    f.read_to_string(&mut text);
+    println!("content:\n{}", text);
+
+    let pattern = Regex::new(&config.pattern)?;
+
+    search(pattern, &text);
+    Ok(())
+
+}
+
+fn search(pattern: Regex, text: &str) ->  bool {
+    println!();
+    let found = pattern.is_match(text);
+    if found {
+        println!("pattern found! '{}'", pattern);
+        for cap in pattern.captures_iter(text) {
+            println!("match: {}", &cap[0]);
+        }
+    } else { println!("pattern not found! '{}'", &pattern) }
+    found
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn test_search() {
+        let found = search(
+            Regex::new("fast,\\s+\\w+").unwrap(),
+            "Rust: safe, fast, productive. Pick three."
+        );
+        assert!(found, "pattern not found!")
+    }
+
+    #[test]
+    fn neg_test_search() {
+        let found = search(
+            Regex::new("Java").unwrap(),
+            "Rust: safe, fast, productive. Pick three."
+        );
+        assert!(!found, "non existing pattern was found!")
+    }
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,52 +1,10 @@
 extern crate regex;
+extern crate minigrep;
 
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
-use std::error::Error;
-use regex::Regex;
+use std::process;
 
-struct Config {
-    pattern: String,
-    filename: String
-}
-
-impl Config {
-    fn new(args: &Vec<String>) -> Result<Config, &'static str> {
-        if args.len() != 3 {
-            return Err("wrong number of arguments (2 needed)")
-        }
-        let pattern = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config { pattern, filename })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<Error>> {
-    println!("searching: {}", config.pattern);
-    println!("in file: {}", config.filename);
-
-    let mut f = File::open(&config.filename).expect(
-        &format!("couldn't find file: '{}'", &config.filename)
-    );
-    let mut text = String::new();
-    f.read_to_string(&mut text);
-    println!("content:\n{}", text);
-
-    let pattern = Regex::new(&config.pattern).unwrap();
-
-    println!();
-    if pattern.is_match(&text) {
-        println!("pattern found! '{}'", &pattern);
-        for cap in pattern.captures_iter(&text) {
-            println!("match: {}", &cap[0]);
-        }
-    }
-
-    else { println!("pattern not found! '{}'", &pattern) }
-    Ok(())
-
-}
+use minigrep::*;
 
 fn main() {
 
@@ -54,6 +12,8 @@ fn main() {
     println!("args: {:?}", args);
 
     let config = Config::new(&args).unwrap(); // parse_cli
-    run(config);
-
+    if let Err(e) = run(config) {
+        println!("application error: {}", e);
+        process::exit(1);
+    }
 }
