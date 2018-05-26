a34049623e24ff25aeab50214ae11139444fb70a
koi <koriatoren@gmail.com>
2018-05-26 14:12:42 +0300

    refactored main, added pattern finding logic

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,10 @@
+extern crate regex;
+
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::error::Error;
+use regex::Regex;
 
 struct Config {
     pattern: String,
@@ -18,13 +22,7 @@ impl Config {
     }
 }
 
-fn main() {
-
-    let args: Vec<String> = env::args().collect();
-    println!("args: {:?}", args);
-
-    let config = Config::new(&args).unwrap(); // parse_cli
-
+fn run(config: Config) -> Result<(), Box<Error>> {
     println!("searching: {}", config.pattern);
     println!("in file: {}", config.filename);
 
@@ -34,5 +32,28 @@ fn main() {
     let mut text = String::new();
     f.read_to_string(&mut text);
     println!("content:\n{}", text);
+
+    let pattern = Regex::new(&config.pattern).unwrap();
+
+    println!();
+    if pattern.is_match(&text) {
+        println!("pattern found! '{}'", &pattern);
+        for cap in pattern.captures_iter(&text) {
+            println!("match: {}", &cap[0]);
+        }
+    }
+
+    else { println!("pattern not found! '{}'", &pattern) }
+    Ok(())
+
+}
+
+fn main() {
+
+    let args: Vec<String> = env::args().collect();
+    println!("args: {:?}", args);
+
+    let config = Config::new(&args).unwrap(); // parse_cli
+    run(config);
 
 }
