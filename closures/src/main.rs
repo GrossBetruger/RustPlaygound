9d97e4f5860187a4ac512a381938d5dbcd8010be
Oren Koriat <oren@cynerio.co>
2018-06-03 15:49:05 +0300

    added proper caching and test for caching

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,12 +1,12 @@
 use std::thread;
 use std::time::Duration;
-
+use std::collections::HashMap;
 
 struct Cacher<T>
     where T: Fn(u32) -> u32
 {
     calculation: T,
-    value: Option<u32>
+    value_map: HashMap<u32, u32>
 }
 
 impl <T> Cacher <T>
@@ -16,20 +16,21 @@ impl <T> Cacher <T>
     fn new(calculation: T) -> Cacher<T> {
         Cacher {
             calculation,
-            value: None
+            value_map: HashMap::new()
         }
     }
 
     fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(val) => val,
-            None => {
-                let val = (self.calculation)(arg);
-                self.value = Some(val);
-                val
-            }
+        match self.value_map.get(&arg) {
+            Some(val) => return *val,
+            None => {}
         }
-    }
+        *self.value_map.entry(arg)
+            .or_insert({
+                       let val = (self.calculation)(arg);
+                       val
+                       })
+        }
 
     }
 
@@ -74,6 +75,34 @@ fn main() {
     generate_workout(
         10, 7
     )
+}
 
+#[test]
+fn cacher_different_args() {
+    let mut cacher = Cacher::new(|num: u32| {
+        println!("input: {}, takes time...", num);
+        thread::sleep(Duration::from_secs(1));
+        num + 0
+    });
+
+    let inpt1 = 1;
+    let inpt2 = 2;
+    let v1 = cacher.value(inpt1);
+    let v2 = cacher.value(inpt2);
+    let v3 = cacher.value(inpt2);
+    let v4 = cacher.value(inpt2);
+    let v5 = cacher.value(inpt2);
+    let v6 = cacher.value(inpt2);
+    let v7 = cacher.value(inpt2);
+    let v8 = cacher.value(inpt2);
+
+    assert_eq!(v1, 1);
+    assert_eq!(v2, 2);
+    assert_eq!(v3, 2);
+    assert_eq!(v4, 2);
+    assert_eq!(v5, 2);
+    assert_eq!(v6, 2);
+    assert_eq!(v7, 2);
+    assert_eq!(v8, 2);
 
 }
