bef60eb9cc6bcd3070e8bb11228926841ef3bbb4
Oren Koriat <oren@cynerio.co>
2018-06-04 13:48:49 +0300

    bug fix

--- network_captures/src/main.rs
+++ network_captures/src/main.rs
@@ -4,6 +4,7 @@ use pcap::Device;
 fn main() {
     let mut cap = Device::lookup().unwrap().open().unwrap();
 
+    let main_device = Device::lookup().unwrap();
     println!("main device: {:?}", main_device);
 
     while let Ok(packet) = cap.next() {
