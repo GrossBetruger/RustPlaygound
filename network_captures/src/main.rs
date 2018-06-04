71b5e2be3127b6aa35d63a679b9ec5f5ae46192b
Oren Koriat <oren@cynerio.co>
2018-06-04 13:47:42 +0300

    added main device printing

--- network_captures/src/main.rs
+++ network_captures/src/main.rs
@@ -4,6 +4,8 @@ use pcap::Device;
 fn main() {
     let mut cap = Device::lookup().unwrap().open().unwrap();
 
+    println!("main device: {:?}", main_device);
+
     while let Ok(packet) = cap.next() {
         println!("received packet! {:?}", packet);
     }
