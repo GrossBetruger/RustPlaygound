258e594597aa86228f430687681a2c3bb0ea31e9
koi <koriatoren@gmail.com>
2018-09-01 14:26:35 +0300

    mutexes

--- fearless_concurrency/src/main.rs
+++ fearless_concurrency/src/main.rs
@@ -1,7 +1,7 @@
 use std::thread;
 use std::time::Duration;
 use std::sync::mpsc;
-
+use std::sync::{Mutex, Arc};
 
 fn main() {
     let handle = thread::spawn(||
@@ -81,5 +81,38 @@ fn main() {
     for rec in rx {
         println!("{}", rec);
     }
+
+    // Shared Memory
+
+    //simple singlethreaded example:
+    let m = Mutex::new(3.141);
+
+    {
+        let mut num = m.lock().unwrap(); // acquire lock to change 3.141 to zero
+        *num = 0.;
+    }
+
+    println!("num: {:?}", m);
+
+    //multithreaded example:
+
+    let counter = Arc::new(Mutex::new(0)); //Atomic reference counter (like Rc but atomic)
+
+    let mut handles = vec![];
+
+    for i in 0..10 {
+        let counter = Arc::clone(&counter); // clone here to avoid 'capture' error
+        let handle = thread::spawn(move || {
+            let mut c = counter.lock().unwrap();
+            *c += 1;
+        });
+        handles.push(handle)
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("final count: '{}'", counter.lock().unwrap());
 
 }
