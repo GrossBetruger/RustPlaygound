42a854917d27aa22c39ab38b0cc170d719b4f6dd
Oren Koriat <oren@cynerio.co>
2018-09-03 15:03:32 +0300

    fuller solution

--- grouped_names/src/main.rs
+++ grouped_names/src/main.rs
@@ -2,21 +2,18 @@ extern crate regex;
 
 fn main() {
     let re = regex::Regex::new(r"(?P<lower>[0-5]+)|(?P<higher>[6-9]+)").unwrap();
-    
+
     let mut groups = vec![];
     for name in re.capture_names() {
         if name.is_some() {
             groups.push(name.unwrap());
         }
     }
-    
-    re.is_match("22 96");
-    
-    let res: Vec<(String, u32)> = vec![];
-    
+
     for cap in re.captures_iter("22 96") {
         let found: Vec<&str> = groups.iter().filter(|x| cap.name(x).is_some()).map(|x| *x).collect();
-        println!("{:?}, offset {:?}", found, cap.get(0));
-        
+        for (i, found_name) in found.into_iter().enumerate() {
+            println!("{:?} {:?}", found_name, cap.get(i));
+        }
     }
 }
