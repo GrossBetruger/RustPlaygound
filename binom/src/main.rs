f851bd15499f5d99260dcb990e65af090f21dfac
Oren Koriat <oren@cynerio.co>
2018-09-05 10:36:45 +0300

    added binom probability of at least k events

--- binom/src/main.rs
+++ binom/src/main.rs
@@ -18,6 +18,19 @@ fn binom(p: f64, n: f64, k: f64) -> f64{
     n_choose_k(n, k) * p.powf(k) * (1. - p).powf(n - k)
 }
 
+/// binom_k_larger_equals.
+///
+/// Description.
+///
+/// calculates the probability of 'k  or more' wanted events.
+///
+/// example: the probability of success in 4 or more out of 5 startups when success probability is
+/// 0.1 => binom_k_larger_equals(0.1, 5., 4.)
+fn binom_k_larger_equals(p: f64, n: f64, k: f64) -> f64 {
+    let chance: f64 = (k as u64..n as u64+1_u64).collect::<Vec<u64>>().iter().map(|_k| binom(p, n, *_k as f64)).sum();
+    chance
+}
+
 fn main() {
     println!("fac 3: {}", factorial(3.));
     println!("fac 4: {}", factorial(4.));
@@ -31,5 +44,6 @@ fn main() {
 
     println!("binom of 3 girls and one boy in family of 5: {}", binom(0.5, 5., 3.));
     println!("80% success rate in 5 startups: {}", binom(STARTUP_SUCCESS_RATE, 5., 4.));
+    println!("at least 80% success rate in 5 startups: {}", binom_k_larger_equals(STARTUP_SUCCESS_RATE, 5., 4.));
 
 }
