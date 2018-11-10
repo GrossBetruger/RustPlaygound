3c3505264b4fe26d819559d33567ee8ac4014deb
koi <koriatoren@gmail.com>
2018-11-10 14:56:46 +0200

    added 404-not-found logic

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -6,7 +6,10 @@ use std::path::Path;
 
 const STATIC_PATH: &str = "static";
 const WELCOME_HTML: &str = "welcome.html";
+const NOT_FOUND_HTML: &str = "404.html";
 const GET: &str = "GET / HTTP/1.1\r\n";
+const STATUS_OK: &str = "HTTP/1.1 200 OK\r\n\r\n";
+const STATUS_NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
 
 fn main() {
 
@@ -26,7 +29,11 @@ fn handle(mut stream: TcpStream) {
     println!("message:\n\n{}", message);
 
     if message.starts_with(GET) {
-        get_page(WELCOME_HTML, &mut stream);
+        deliver_page(STATUS_OK, WELCOME_HTML, &mut stream);
+    }
+
+    else {
+        deliver_page(STATUS_NOT_FOUND, NOT_FOUND_HTML, &mut stream)
     }
 }
 
@@ -34,11 +41,13 @@ fn append_html(http_response: &str, html_path: &str) -> String {
     format!("{}{}", http_response, fs::read_to_string(Path::new(STATIC_PATH).join(html_path)).unwrap())
 }
 
-fn get_page(page_name: &str, stream: &mut TcpStream) {
-    let response = append_html("HTTP/1.1 200 OK\r\n\r\n", page_name);
+fn deliver_page(status_line: &str, page_name: &str, stream: &mut TcpStream) {
+    let response = append_html(status_line, page_name);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
     println!("response sent successfully");
 }
+
+
 
 
