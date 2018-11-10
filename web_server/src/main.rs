e15e1042b401fc270e5b0ad66620923ba779d3b3
koi <koriatoren@gmail.com>
2018-11-10 14:49:25 +0200

    handle connection according to http request type

--- web_server/src/main.rs
+++ web_server/src/main.rs
@@ -5,6 +5,8 @@ use std::fs;
 use std::path::Path;
 
 const STATIC_PATH: &str = "static";
+const WELCOME_HTML: &str = "welcome.html";
+const GET: &str = "GET / HTTP/1.1\r\n";
 
 fn main() {
 
@@ -20,17 +22,23 @@ fn main() {
 fn handle(mut stream: TcpStream) {
     let mut buf = [0u8; 512];
     stream.read(&mut buf).unwrap();
-    println!("message:\n\n{}", String::from_utf8_lossy(&buf[..]));
+    let message = String::from_utf8_lossy(&buf[..]);
+    println!("message:\n\n{}", message);
 
-    let response = append_html("HTTP/1.1 200 OK\r\n\r\n", "welcome.html");
-
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-    println!("response sent successfully");
+    if message.starts_with(GET) {
+        get_page(WELCOME_HTML, &mut stream);
+    }
 }
 
 fn append_html(http_response: &str, html_path: &str) -> String {
     format!("{}{}", http_response, fs::read_to_string(Path::new(STATIC_PATH).join(html_path)).unwrap())
+}
+
+fn get_page(page_name: &str, stream: &mut TcpStream) {
+    let response = append_html("HTTP/1.1 200 OK\r\n\r\n", page_name);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+    println!("response sent successfully");
 }
 
 
