58545dc3258eca62d3cfb6edf33b31c612b39ff2
koi <koriatoren@gmail.com>
2018-11-16 21:38:07 +0200

    macros: higher order macro: printlines

--- macros/src/main.rs
+++ macros/src/main.rs
@@ -2,7 +2,7 @@
 
 fn main() {
     #[macro_export]
-    macro_rules! vec {
+    macro_rules! vector {
         ( $( $x:expr),* ) => {
             {
                let mut vec = Vec::new();
@@ -15,6 +15,19 @@ fn main() {
         };
     }
 
+    // Higher Order Macro:
+    macro_rules! printlines {
+        ($ ($x:expr),*) => {
+            {
+                $(println!("{}", $x);)*
+            }
+        };
+    }
 
-    println!("macro created vec: {:?}", vec![1,2,3])
+
+    println!("macro created vec: {:?}", vector![1,2,3]);
+
+    printlines!("line: 1", "line: 2", "line: 3");
+
+
 }
